f403eb13dbbd1fe8d88d435990feb00b30199b78
Keith Bartlett <kieth.tienyou.bartlett@gmail.com>
2021-06-07 22:48:47 -0700

    fixed memory issues with some questionable code. (but it works)

--- new/js/backgrounds/bg-faces.js
+++ new/js/backgrounds/bg-faces.js
@@ -1,51 +1,35 @@
 const infoText = document.getElementById("bg-info");
-document.getElementById("bg-name").innerHTML = "bg: faces";
-infoText.innerHTML = "simplex, going to be used to control face parameters";
-var cellSize = 256;
+document.getElementById("bg-name").innerHTML = "bg: faces-vae";
 
-const bgColor = "#0a0a0a";
+infoText.innerHTML = "a variational autoencoder i trained on my freshman yearbook controlled by 3d noise";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
-ctx.imageSmoothingEnabled = false;
+ctx.fillStyle = "#0a0a0a"
+ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
 const bigCanvas = document.createElement("canvas");
-bigCanvas.width = cellSize
-bigCanvas.height = cellSize
+bigCanvas.width = 256
+bigCanvas.height = 256
 const bigCtx = bigCanvas.getContext('2d')
-bigCtx.imageSmoothingEnabled = false;
-
-var curRow = 0;
-var map = [];
-var time = 0;
-var decoder = null;
-var canDecode = false;
-var curCellX = 0;
-var curCellY = 0;
-var loops = 0;
-var fadeAlpha = 0;
 
 function init() {
     window.addEventListener('resize', resizeCanvas);
     resizeCanvas();
 
     noise.seed(Math.random());
-    loadModel();
-
-    ctx.fillStyle = bgColor
-    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-    setTimeout(
-        update
-    , 10)
-    //update();
+    update(0, 0, 0)
 };
-init();
 
 async function loadModel() {
+    tf.ENV.set('WEBGL_CPU_FORWARD', false);
+
     decoder = await tf.loadLayersModel('./models/faces/decoder/model.json');
     console.log("Model Loaded");
-    canDecode = true;
-    
+    init();
 }
+
+loadModel();
 
 function drawFromRGB(arr, px, py) {
     for (var y = 0; y < 32; y++)
@@ -54,12 +38,11 @@ function drawFromRGB(arr, px, py) {
         {
             ctx.fillStyle = `rgb(${Math.round((arr[0][y][x][0] + 1.9) * 65)}, ${Math.round((arr[0][y][x][1] + 1.9) * 65)}, ${Math.round((arr[0][y][x][2] + 1.9) * 65)})`
             ctx.fillRect(px + x*8, py + y*8, 8, 8);
-            //console.log(`rgb(${Math.round((arr[0][y][x][0] + 1.9) * 65)}, ${Math.round((arr[0][y][x][2] + 1.9) * 65)}, ${Math.round((arr[0][y][x][2] + 1.9) * 65)})`)
         }
     }
 }
 
-function drawFromRGB_imdata(arr, px, py) {
+function drawFromRGB_imdata(arr, px, py, fadeAlpha) {
     ctx.setTransform(10,0,0,10,0,0);
 
     arr = arr[0]
@@ -85,71 +68,60 @@ function drawFromRGB_imdata(arr, px, py) {
 function resizeCanvas() {
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
-    cellSize = 256;
-    scaleMap(Math.ceil(canvas.width/cellSize), Math.ceil(canvas.height/cellSize));
-}
-
-function scaleMap(newSizeX, newSizeY) {
-    map = Array.from(Array(newSizeY), () => new Array(newSizeX).fill([Array.from({length: 20}, () => 0)]))
-    ctx.fillStyle = bgColor
+    ctx.fillStyle = "#0a0a0a"
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 }
 
-function draw() {
-    if (canDecode) {
-        if (curCellY+1 > Math.ceil(window.innerHeight/cellSize)) {
-            curCellX += 1
-            curCellY = 0
-            console.log("ayo wrap to next col")
-
-            if (curCellX+1 > Math.ceil(window.innerWidth/cellSize)) {
-                loops += 1
-                console.log("ayo wrap back around")
-                curCellX = 0
+function drawRow(time, curCellX, fadeAlpha) {
+    if (decoder != null) {
+        for (var y = 0; y < Math.ceil(window.innerHeight/256); y++) {
+            if (curCellX+1 > Math.ceil(window.innerWidth/256)) {
+                curCellX = 0;
             }
-        }
-        for (var i = 0; i < 20; i++) { map[curCellY][curCellX][0][i] = noise.simplex3((curCellX/10)+i, (curCellY/10)+i, time)*2; }
-        decode(map[curCellY][curCellX], curCellX * cellSize, curCellY * cellSize);
-        curCellY += 1
-        ctx.strokeStyle = `yellow`
-        ctx.lineWidth = 8;
-        ctx.strokeRect(curCellX * cellSize+4, curCellY * cellSize+4, cellSize-8, cellSize-8);
-    }
-}
-
-function drawRow() {
-    if (canDecode){
-        for (var y = 0; y < Math.ceil(window.innerHeight/cellSize); y++) {
-            curCellY = y
-
-            if (curCellX+1 > Math.ceil(window.innerWidth/cellSize)) {
-                loops += 1
-                curCellX = 0
-            }
-            for (var i = 0; i < 20; i++) { map[y][curCellX][0][i] = noise.simplex3((curCellX/7)+i, (y/7)+i, time)*2; }
-            decode(map[y][curCellX], curCellX * 32, y * 32);
+            tf.tidy(()=>{
+                decode([[noise.simplex3((curCellX/7)+0, (y/7)+0, time)*2,
+                        noise.simplex3((curCellX/7)+1, (y/7)+1, time)*2,
+                        noise.simplex3((curCellX/7)+2, (y/7)+2, time)*2,
+                        noise.simplex3((curCellX/7)+3, (y/7)+3, time)*2,
+                        noise.simplex3((curCellX/7)+4, (y/7)+4, time)*2,
+                        noise.simplex3((curCellX/7)+5, (y/7)+5, time)*2,
+                        noise.simplex3((curCellX/7)+6, (y/7)+6, time)*2,
+                        noise.simplex3((curCellX/7)+7, (y/7)+7, time)*2,
+                        noise.simplex3((curCellX/7)+8, (y/7)+8, time)*2,
+                        noise.simplex3((curCellX/7)+9, (y/7)+9, time)*2,
+                        noise.simplex3((curCellX/7)+10, (y/7)+10, time)*2,
+                        noise.simplex3((curCellX/7)+11, (y/7)+11, time)*2,
+                        noise.simplex3((curCellX/7)+12, (y/7)+12, time)*2,
+                        noise.simplex3((curCellX/7)+13, (y/7)+13, time)*2,
+                        noise.simplex3((curCellX/7)+14, (y/7)+14, time)*2,
+                        noise.simplex3((curCellX/7)+15, (y/7)+15, time)*2,
+                        noise.simplex3((curCellX/7)+16, (y/7)+16, time)*2,
+                        noise.simplex3((curCellX/7)+17, (y/7)+17, time)*2,
+                        noise.simplex3((curCellX/7)+18, (y/7)+18, time)*2,
+                        noise.simplex3((curCellX/7)+19, (y/7)+19, time)*2,]], curCellX * 32, y * 32, fadeAlpha);
+            });
         }
         curCellX +=1
     }
-    
+    return curCellX;
 }
 
-async function decode(dense, x, y) {
+async function decode(dense, x, y, fadeAlpha) {
     var rgb = await decoder.predict(tf.tensor(dense)).array();
-    drawFromRGB_imdata(rgb, x, y);
-    delete rgb;
+    drawFromRGB_imdata(rgb, x, y, fadeAlpha);
 }
-function update() {
-    var t0 = performance.now()
+
+function update(fadeAlpha, time, curCellX) {
+    //var t0 = performance.now()
     time += 0.002
-    drawRow();
+    curCellX = drawRow(time, curCellX, fadeAlpha);
     
-    if (canDecode) { 
+    if (decoder != null) { 
         if (fadeAlpha < 0.3) {
             fadeAlpha += 0.01
         }
     }
-    var t1 = performance.now()
+    //var t1 = performance.now()
     //console.log(t1-t0)
-    setTimeout(update)
+    setTimeout(()=>{update(fadeAlpha, time, curCellX)})
 }
