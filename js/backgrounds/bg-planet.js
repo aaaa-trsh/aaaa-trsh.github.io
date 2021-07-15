1ddb429c374c7f254df1d6dc62fb07ecd4c9989c
Keith Bartlett <kieth.tienyou.bartlett@gmail.com>
2021-07-14 18:56:48 -0700

    more finished planet

--- js/backgrounds/bg-planet.js
+++ js/backgrounds/bg-planet.js
@@ -1,8 +1,8 @@
 let camera, scene, renderer, controls, stats;
-let target;
-let postScene, postCamera, postMaterial, planetMaterial;
+let postRenderTarget, imageRenderTarget;
+let postScene, postCamera, postMaterial, planetMaterial, oceanMaterial;
 let supportsExtension = true;
-let sphere, sphereGeometry;
+let planet, planetGeometry;
 let SHADER = 0 == 1;
 let noise;
 init();
@@ -39,21 +39,25 @@ function init() {
 }
 
 function setupRenderTarget() {
-    if (target) target.dispose();
+    if (postRenderTarget) postRenderTarget.dispose();
+    if (imageRenderTarget) imageRenderTarget.dispose();
 
     const format = THREE.DepthFormat;
     const type = THREE.UnsignedShortType;
 
-    target = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
-    target.texture.format = THREE.RGBFormat;
-    target.texture.minFilter = THREE.NearestFilter;
-    target.texture.magFilter = THREE.NearestFilter;
-    target.texture.generateMipmaps = false;
-    target.stencilBuffer = (format === THREE.DepthStencilFormat) ? true : false;
-    target.depthBuffer = true;
-    target.depthTexture = new THREE.DepthTexture();
-    target.depthTexture.format = format;
-    target.depthTexture.type = type;
+    postRenderTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+    postRenderTarget.texture.format = THREE.RGBFormat;
+    postRenderTarget.texture.minFilter = THREE.NearestFilter;
+    postRenderTarget.texture.magFilter = THREE.NearestFilter;
+    postRenderTarget.texture.generateMipmaps = false;
+    postRenderTarget.stencilBuffer = (format === THREE.DepthStencilFormat) ? true : false;
+    postRenderTarget.depthBuffer = true;
+    postRenderTarget.depthTexture = new THREE.DepthTexture();
+    postRenderTarget.depthTexture.format = format;
+    postRenderTarget.depthTexture.type = type;
+
+    imageRenderTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+    imageRenderTarget.copy(postRenderTarget);
 }
 
 function deg2rad(deg) { return deg * Math.PI / 180; }
@@ -88,7 +92,8 @@ function setupPost() {
             matrixWorld: { value: camera.matrixWorld },
             viewportDimensions: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
             tDiffuse: { value: null },
-            tDepth: { value: null }
+            tDepth: { value: null },
+            startTime: { value: Date.now() }
         }
     });
     const postPlane = new THREE.PlaneGeometry(2, 2);
@@ -112,13 +117,22 @@ function setupScene() {
     planetMaterial = new THREE.ShaderMaterial({
         vertexShader: document.getElementById('sphereVertShader').textContent.trim(),
         fragmentShader: document.getElementById('sphereFragShader').textContent.trim(),
+    });
+    planetGeometry = new THREE.IcosahedronGeometry(.6, 15);
+    planet = new THREE.Mesh(planetGeometry, planetMaterial);
+    scene.add(planet);
+    
+    oceanGeometry = new THREE.IcosahedronGeometry(.72, 4);
+    oceanMaterial = new THREE.ShaderMaterial({
+        vertexShader: document.getElementById('oceanVertShader').textContent.trim(),
+        fragmentShader: document.getElementById('oceanFragShader').textContent.trim(),
         uniforms: {
-            tDiffuse: { value: null }
-        }
-    });//new THREE.MeshStandardMaterial({ color: 'white' });
-    sphereGeometry = new THREE.SphereGeometry(.6, 64, 64);
-    sphere = new THREE.Mesh(sphereGeometry, planetMaterial);
-    scene.add(sphere);
+            tDiffuse: { value: null },
+        },
+        transparent: true
+    })
+    oceanSphere = new THREE.Mesh(oceanGeometry, oceanMaterial);
+    scene.add(oceanSphere);
 }
 
 function onWindowResize() {
@@ -127,7 +141,7 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     const dpr = renderer.getPixelRatio();
-    target.setSize(window.innerWidth * dpr, window.innerHeight * dpr);
+    postRenderTarget.setSize(window.innerWidth * dpr, window.innerHeight * dpr);
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
@@ -137,7 +151,11 @@ function update() {
 
     requestAnimationFrame(update);
 
-    renderer.setRenderTarget(target);
+    //renderer.setRenderTarget(imageRenderTarget);
+    //renderer.render(scene, camera);
+    //oceanMaterial.uniforms.tDiffuse.value = imageRenderTarget.texture;
+
+    renderer.setRenderTarget(postRenderTarget);
     renderer.render(scene, camera);
 
     let camDir = new THREE.Vector3();
@@ -150,16 +168,14 @@ function update() {
     postMaterial.uniforms.cameraDirection.value = camDir;
     postMaterial.uniforms.cameraPos.value = camera.position;
     postMaterial.uniforms.cameraDistance.value = camera.position.distanceTo(new THREE.Vector3());
-    postMaterial.uniforms.tDiffuse.value = target.texture;
-    planetMaterial.uniforms.tDiffuse.value = target.texture;
-    postMaterial.uniforms.tDepth.value = target.depthTexture;
+    postMaterial.uniforms.tDiffuse.value = postRenderTarget.texture;
+    postMaterial.uniforms.tDepth.value = postRenderTarget.depthTexture;
     postMaterial.uniforms.upperLeft.value = upperLeft;    
     postMaterial.uniforms.upperRight.value = upperRight;    
     postMaterial.uniforms.lowerLeft.value = lowerLeft;    
     postMaterial.uniforms.lowerRight.value = lowerRight;
 
     renderer.setRenderTarget(null);
-
     renderer.render(postScene, postCamera);
 
     controls.update();
